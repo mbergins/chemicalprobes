981904ec41cfd473fb9ebb6629afae0213e0a5e2
Matthew Berginski <matthew.berginski@gmail.com>
2019-06-19 15:26:35 -0400

    updates to dataset processing

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -14,11 +14,13 @@ chemical_probes <- htmltab::htmltab('http://www.chemicalprobes.org/browse_probes
 #  clipr::write_clip(chemical_probes$protein_target)
 
 HGNC_name_matches = read_csv(here('data-raw/hgnc-symbol-check.csv'), skip=1) %>%
-  janitor::clean_names()
+  janitor::clean_names() %>%
+  filter(match_type == "Approved symbol")
 
 chemical_probes = chemical_probes %>%
   left_join(HGNC_name_matches %>% select(input,approved_symbol),
             by=c('protein_target' = 'input')) %>%
-  rename(hgnc_symbol = approved_symbol)
+  rename(hgnc_symbol = approved_symbol) %>%
+  arrange(hgnc_symbol)
 
 usethis::use_data(chemical_probes, overwrite = T)
