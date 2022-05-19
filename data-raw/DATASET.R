6cd35abc4bb844f9335029d840143dcb37781ee5
Matthew Berginski <matthew.berginski@gmail.com>
2022-05-19 10:07:59 -0400

    update to the latest chemical probes list

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,11 +1,12 @@
-## code to prepare `DATASET` dataset goes here
 library(tidyverse)
 library(here)
+library(janitor)
 
-chemical_probes <- htmltab::htmltab('http://www.chemicalprobes.org/browse_probes', which = 1) %>%
-  janitor::clean_names() %>%
+chemical_probes <- read_csv(here('data-raw/ChemicalProbesPortal-5_19_2022.csv')) %>%
+  clean_names() %>%
   separate_rows("target_name", sep=",") %>%
-  mutate(target_name = trimws(target_name))
+  mutate(target_name = trimws(target_name)) %>%
+  filter(probe_name != "Please cite the Chemical Probes Portal")
 
 #I submitted the above "target_name" column through HGNC's multi symbol checker:
 #  https://www.genenames.org/tools/multi-symbol-checker/
@@ -14,7 +15,7 @@ chemical_probes <- htmltab::htmltab('http://www.chemicalprobes.org/browse_probes
 #  clipr::write_clip(chemical_probes$target_name)
 
 HGNC_name_matches = read_csv(here('data-raw/hgnc-symbol-check.csv'), skip=1) %>%
-  janitor::clean_names() %>%
+  clean_names() %>%
   filter(match_type == "Approved symbol")
 
 chemical_probes = chemical_probes %>%
